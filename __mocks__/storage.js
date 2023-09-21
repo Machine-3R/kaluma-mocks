43abb7d2a5bdda045b159cf60377dfef1f8522de
Machine-3R <github@lanenga.net>
2023-09-21 21:44:36 +0200

    added fs emulation for Storage

--- __mocks__/storage.js
+++ __mocks__/storage.js
@@ -1,23 +1,26 @@
 'use strict';
 
+const fs = require('fs');
+
 class Storage {
     _instance = null;
-    _items = [];
+    #items = [];
+    #path = process.cwd() + '/storage.json';
 
     constructor() {
         if (Storage._instance) {
             return Storage._instance;
         }
         Storage._instance = this;
+        this.#writeItems();    
     }
 
-
     get length() {
-        return this._items.length;
+        return this.#items.length;
     }
 
     getItem(key) {
-        return this._items.find((el) => el.key === key);
+        return this.#items.find((el) => el.key === key);
     }
 
     setItem(key, value) {
@@ -26,25 +29,53 @@ class Storage {
             item.value = value;
         } else {
             item = { key, value };
-            this._items.push(item);
+            this.#items.push(item);
         }
+        this.#writeItems();
     }
 
     removeItem(key) {
-        let index = this._items.findIndex((el) => el.key === key);
-        this._items.splice(index, 1);
+        let index = this.#items.findIndex((el) => el.key === key);
+        this.#items.splice(index, 1);
+        this.#writeItems()
     }
 
     clear() {
-        this._items.length = 0;
+        this.#items.length = 0;
+        this.#deleteItems();
     }
 
+    #readItems() {
+        fs.readFileSync(this.#path, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+            this.#items = JSON.parse(data);
+        });
+    }
+
+    #writeItems() {
+        let json = JSON.stringify(this.#items);
+        fs.writeFileSync(this.#path, json, 'utf8', (err) => {
+            if (err) {
+                throw err;
+            }
+            this.#readItems();
+        });
+    }
+
+    #deleteItems() {
+        fs.unlinkSync(this.#path, (err)=>{
+            if (err) {
+                throw err;
+            }
+        });
+    }
 }
 Storage.getInstance = function () {
     "use strict";
     return Storage._instance || new Storage();
 }
-
 
 module.exports = {
     Storage
