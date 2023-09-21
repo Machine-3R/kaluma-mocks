c0bac906f20df123890d6eb352b8de5defc52cf9
Machine-3R <github@lanenga.net>
2023-09-21 21:31:36 +0200

    added events

--- __mocks__/wifi.js
+++ __mocks__/wifi.js
@@ -20,14 +20,16 @@ class WiFi extends EventEmitter {
     }
 
     connect(connectInfo = ConnectInfo, cb = function () { }) {
+        this.emit('associated'); // This event is emitted when Wi-Fi network is connected. IP may not be assigned.
+        this.emit('connected'); // This event is emitted when Wi-Fi network is connected with IP assignment.
     }
 
     disconnect(cb = function () { }) {
+        this.emit('disconnected'); // This event is emitted when Wi-Fi network is disconnected.
     }
 
     getConnection(cb = function () { }) {
     }
-
 }
 
 module.exports = {
