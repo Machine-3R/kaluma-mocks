906ef8c2c6ed64023ed5aa617327f551543bd7dc
Machine-3R <github@lanenga.net>
2023-09-21 21:44:55 +0200

    refactored code

--- test/kaluma-mocks.test.js
+++ test/kaluma-mocks.test.js
@@ -3,115 +3,115 @@
 jest.useFakeTimers();
 
 it('requires adc', function () {
-    expect(()=>require('adc')).not.toThrow();
+    expect(() => require('adc')).not.toThrow();
 });
 
 it('requires analog-io', function () {
-    expect(()=>require('analog-io')).not.toThrow();
+    expect(() => require('analog-io')).not.toThrow();
 });
 
 it('requires at', function () {
-    expect(()=>require('at')).not.toThrow();
+    expect(() => require('at')).not.toThrow();
 });
 
 it('requires board', function () {
-    expect(()=>require('board')).not.toThrow();
+    expect(() => require('board')).not.toThrow();
 });
 
 it('requires button', function () {
-    expect(()=>require('button')).not.toThrow();
+    expect(() => require('button')).not.toThrow();
 });
 
 it('requires console', function () {
-    expect(()=>require('console')).not.toThrow();
+    expect(() => require('console')).not.toThrow();
 });
 
 it('requires pico_cyw43', function () {
-    expect(()=>require('pico_cyw43')).not.toThrow();
+    expect(() => require('pico_cyw43')).not.toThrow();
 });
 
 it('requires device-driver', function () {
-    expect(()=>require('device-driver')).not.toThrow();
+    expect(() => require('device-driver')).not.toThrow();
 });
 
 it('requires digital-io', function () {
-    expect(()=>require('digital-io')).not.toThrow();
+    expect(() => require('digital-io')).not.toThrow();
 });
 
 it('requires errors', function () {
-    expect(()=>require('errors')).not.toThrow();
+    expect(() => require('errors')).not.toThrow();
 });
 
 it('requires events', function () {
-    expect(()=>require('events')).not.toThrow();
+    expect(() => require('events')).not.toThrow();
 });
 
 it('requires fs', function () {
-    expect(()=>require('fs')).not.toThrow();
+    expect(() => require('fs')).not.toThrow();
 });
 
 it('requires flash', function () {
-    expect(()=>require('flash')).not.toThrow();
+    expect(() => require('flash')).not.toThrow();
 });
 
 it('requires global', function () {
-    expect(()=>require('global')).not.toThrow();
+    expect(() => require('global')).not.toThrow();
 });
 
 it('requires gpio', function () {
-    expect(()=>require('gpio')).not.toThrow();
+    expect(() => require('gpio')).not.toThrow();
 });
 
 it('requires graphics', function () {
-    expect(()=>require('graphics')).not.toThrow();
+    expect(() => require('graphics')).not.toThrow();
 });
 
 it('requires http', function () {
-    expect(()=>require('http')).not.toThrow();
+    expect(() => require('http')).not.toThrow();
 });
 
 it('requires i2c', function () {
-    expect(()=>require('i2c')).not.toThrow();
+    expect(() => require('i2c')).not.toThrow();
 });
 
 it('requires interrupts', function () {
-    expect(()=>require('interrupts')).not.toThrow();
+    expect(() => require('interrupts')).not.toThrow();
 });
 
 it('requires led', function () {
-    expect(()=>require('led')).not.toThrow();
+    expect(() => require('led')).not.toThrow();
 });
 
 it('requires net', function () {
-    expect(()=>require('net')).not.toThrow();
+    expect(() => require('net')).not.toThrow();
 });
 
 it('requires path', function () {
-    expect(()=>require('path')).not.toThrow();
+    expect(() => require('path')).not.toThrow();
 });
 
 it('requires process', function () {
-    expect(()=>require('process')).not.toThrow();
+    expect(() => require('process')).not.toThrow();
 });
 
 it('requires pwm', function () {
-    expect(()=>require('pwm')).not.toThrow();
+    expect(() => require('pwm')).not.toThrow();
 });
 
 it('requires rp2', function () {
-    expect(()=>require('rp2')).not.toThrow();
+    expect(() => require('rp2')).not.toThrow();
 });
 
 it('requires rtc', function () {
-    expect(()=>require('rtc')).not.toThrow();
+    expect(() => require('rtc')).not.toThrow();
 });
 
 it('requires sdcard', function () {
-    expect(()=>require('sdcard')).not.toThrow();
+    expect(() => require('sdcard')).not.toThrow();
 });
 
 it('requires spi', function () {
-    expect(()=>require('spi')).not.toThrow();
+    expect(() => require('spi')).not.toThrow();
 });
 
 it('requires storage', function () {
@@ -142,27 +142,27 @@ it('requires storage', function () {
 });
 
 it('requires stream', function () {
-    expect(()=>require('stream')).not.toThrow();
+    expect(() => require('stream')).not.toThrow();
 });
 
 it('requires timers', function () {
-    expect(()=>require('timers')).not.toThrow();
+    expect(() => require('timers')).not.toThrow();
 });
 
 it('requires uart', function () {
-    expect(()=>require('uart')).not.toThrow();
+    expect(() => require('uart')).not.toThrow();
 });
 
 it('requires url', function () {
-    expect(()=>require('url')).not.toThrow();
+    expect(() => require('url')).not.toThrow();
 });
 
 it('requires wdt', function () {
-    expect(()=>require('wdt')).not.toThrow();
+    expect(() => require('wdt')).not.toThrow();
 });
 
 it('requires wifi', function () {
-    expect(()=>require('wifi')).not.toThrow();
+    expect(() => require('wifi')).not.toThrow();
 });
 
 
